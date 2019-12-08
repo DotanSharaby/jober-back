2f2e06935fc810bb7096ddf7f5f828fd801f8e20
DotanSharaby <dotan.sharaby@gmail.com>
2019-12-08 14:59:19 +0200

    before heroku deploy

--- server.js
+++ server.js
@@ -42,7 +42,10 @@ app.use('/api/job', jobRoutes)
 connectSockets(io);
 
 if (process.env.NODE_ENV !== 'development') {
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.resolve(__dirname, 'public')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname, 'public','index.html'))
+    })
 }
 
 const port = process.env.PORT || 3000;
